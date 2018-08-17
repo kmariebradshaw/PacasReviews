ce9085ed8102763345077bc6a0380e80bf521f16
Kaitlyn Bradshaw <kaitlynbradshaw@Blairs-MacBook-Pro-2.local>
2018-08-17 16:53:49 -0500

    adds answers and questions for a survey functionality that is separate from the reviews app but needed

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_01_183320) do
+ActiveRecord::Schema.define(version: 2018_08_17_215024) do
 
   create_table "products", force: :cascade do |t|
     t.string "title", null: false
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "text"
+    t.integer "vote"
+    t.text "response"
   end
 
   create_table "reviews", force: :cascade do |t|
