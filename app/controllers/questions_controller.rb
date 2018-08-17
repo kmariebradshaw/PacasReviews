d98d7efdfc8c536722ca269d7bf09b29bb0c7d11
Kaitlyn Bradshaw <kaitlynbradshaw@Blairs-MacBook-Pro-2.local>
2018-08-17 17:39:33 -0500

    adds creating a question functionality

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,28 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all 
+  end 
+  def new 
+    @question = Question.new
+  end 
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @questions
+    else
+      render 'new'
+    end 
+  end 
+  def show
+    @question = Question.find(params[:id]) 
+  end 
+  def update
+    @question = Question.find(params[:id]) 
+    @question.update(question_params)
+  end 
+  private 
+  def question_params
+      params.require(:question).permit(:text, :response_type)
+  end 
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   resources :products
+  resources :questions
   resources :reviews
     root 'welcome#index'
 
