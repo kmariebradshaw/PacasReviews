b4cefeaefbcb3219fba2457e7e8dec425e322a6d
Kaitlyn Bradshaw <kaitlynbradshaw@Blairs-MacBook-Pro-2.local>
2018-12-17 13:12:23 -0600

    adds a favorites box on the dashboard where users can unfavorite a review

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @reviews = Review.all 
+    @reviews = Review.all.order("created_at DESC")
     @positive = @reviews.where("rating >= ?", 4).count
     @pending_reviews = @reviews.where(:status => "pending")
     @not_pending_reviews = @reviews.where.not(:status => "pending")
