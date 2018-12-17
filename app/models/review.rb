cd98c0fba3b2c0301be8fa5fd58310674ffe49d1
Kaitlyn Bradshaw <kaitlynbradshaw@Blairs-MacBook-Pro-2.local>
2018-12-17 13:02:34 -0600

    changes styling on dashboard page and fixes an issue with a mislabeled column in the csv download

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,9 +7,9 @@ class Review < ApplicationRecord
 
   def self.to_csv
     CSV.generate do |csv|
-      csv << %w{ first_name last_name id date rating text email } 
+      csv << %w{ first_name last_name product date rating text email } 
       all.each do |review|
-        csv << [review.author_first, review.author_last, review.product.title, review.rating, review.text, review.author_email  ]
+        csv << [review.author_first, review.author_last, review.product.title, review.created_at, review.rating, review.text, review.author_email  ]
       end
     end
   end
