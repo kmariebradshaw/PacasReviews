b68e60b95f1ee687ac9b72d6ab71976c3e17e60d
Kaitlyn Bradshaw <kaitlynbradshaw@Blairs-MacBook-Pro-2.local>
2018-12-18 10:55:21 -0600

    updates routes to make them unreachable by unauthenticated users and creates a nav for signed in users

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/index'
-  resources :products
-  resources :questions
-  resources :reviews
-  resources :answers
-  resources :responses
-    root 'welcome#index'
 
+  authenticated :user do
+    root to: 'welcome#index', as: :authenticated_root
+    resources :products
+    resources :questions
+    resources :reviews
+    resources :answers
+    resources :responses
+  end
+  resources :reviews, only: [:new, :show, :index, :create]
+  root to: redirect('/users/sign_in')
 end
