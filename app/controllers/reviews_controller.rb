fa0aa7a6830510883aef2bf7368e8e08d9325ea4
Kaitlyn Bradshaw <kaitlynbradshaw@Blairs-MacBook-Pro-2.local>
2018-12-20 14:51:20 -0600

    adds in the review count

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -18,6 +18,7 @@ class ReviewsController < ApplicationController
     @users = User.all 
     @reviews = Review.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 250)
     @favorited_reviews = Review.all.where(:staff_favorite => true)
+    @positive_review_count = Review.where("rating > ?", 3).count()
    respond_to do |format|
     format.html
     format.csv { send_data @reviews.to_csv, filename: "Reviews-#{Date.today}.csv" }
