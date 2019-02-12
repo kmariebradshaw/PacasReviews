4049992e3912e2263ee1664df2519b957e046a7b
Kaitlyn Bradshaw <kaitlynbradshaw@Blairs-MacBook-Pro-2.local>
2019-02-12 11:40:03 -0600

    comments out staff favorites as the functionality will be used eventually but not until we have significantly more reviews

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,7 @@ class WelcomeController < ApplicationController
     @favorited_reviews = Review.all.where(:staff_favorite => true)
     @positive = @reviews.where("rating >= ?", 4).count
     @pending_reviews = Review.all.where(:status => "pending")
+    @latest_reviews = Review.all.where.not(:status => "pending").where(:staff_favorite => false)
   end
   def show 
     @reviews = Review.all.where(:status => "approved").order("created_at DESC").limit(5)
