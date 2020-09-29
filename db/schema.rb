ca6ce34f0777cfd5704d2aebce0548ea4d5a47e9
Kate Bradshaw <kaitlynbradshaw@kaitlyns-mbp.attlocal.net>
2020-09-29 15:00:16 -0500

    updates controller with destory method, removes unnecessary styles from views, adds status to products via new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_223626) do
+ActiveRecord::Schema.define(version: 2020_09_29_195534) do
 
   create_table "answers", force: :cascade do |t|
     t.string "text"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_12_18_223626) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "approved"
   end
 
   create_table "questions", force: :cascade do |t|
