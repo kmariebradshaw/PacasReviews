906c00a4dbfc8c43fc0abecea15813f280fda174
Kaitlyn Bradshaw <kmbradshaw@ymail.com>
2023-04-20 14:05:38 -0500

    limits to 50 results

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,7 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id]) 
 
-    @reviews = @product.reviews.where(:status => "approved").order("created_at DESC").paginate(:page => params[:page], :per_page => 5) 
+    @reviews = @product.reviews.where(:status => "approved").order("created_at DESC").paginate(:page => params[:page], :per_page => 5).limit(50) 
     @positive_review_count = @product.reviews.where("rating > ?", 3).count()
     @no_reviews = Review.all.where(:status => "approved").order("created_at DESC").limit(5)
   end 
